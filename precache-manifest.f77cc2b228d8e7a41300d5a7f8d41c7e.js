f867bd03e2d9f565d353b77bdbed928a3d3b209d
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2018-11-12 22:30:22 +0100

    Updates

---
+++ precache-manifest.f77cc2b228d8e7a41300d5a7f8d41c7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbdbcde4afbfd8b19464b50bacfa1db0",
+    "url": "/Hangman/static/media/gallows.fbdbcde4.png"
+  },
+  {
+    "revision": "ae1caf0167b4b39de989",
+    "url": "/Hangman/static/js/runtime~main.ae1caf01.js"
+  },
+  {
+    "revision": "d91d8f2ca57c439e742e",
+    "url": "/Hangman/static/js/main.d91d8f2c.chunk.js"
+  },
+  {
+    "revision": "68757850f3d8e94aba82",
+    "url": "/Hangman/static/js/1.68757850.chunk.js"
+  },
+  {
+    "revision": "d91d8f2ca57c439e742e",
+    "url": "/Hangman/static/css/main.3ae2ab14.chunk.css"
+  },
+  {
+    "revision": "d55adec54d61fe7ad9f3458d84f9dcc3",
+    "url": "/Hangman/index.html"
+  }
+];
